6c44fd0fa2e290deed7b315af020daa4734e9369
lucasangiorama <angioramaluca@gmail.com>
2021-07-10 18:58:57 -0300

    fixed a4

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(publicPath));
 
 
 const publicPath2 = path.resolve(__dirname, "./views");
-app.use(express.static(publicPath));
+app.use(express.static(publicPath2));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
