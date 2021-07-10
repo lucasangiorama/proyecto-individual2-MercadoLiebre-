057e6539ad0fc605c0114734d0c350ae3e911d4b
lucasangiorama <angioramaluca@gmail.com>
2021-07-10 18:55:06 -0300

    fixed a2

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 
-const publicPath2 = path.resolve(__dirname, "./views");
-app.use(express.static(publicPath));
+const publicPath = path.resolve(__dirname, "./views");
+app.use(express.static(publicPath2));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
